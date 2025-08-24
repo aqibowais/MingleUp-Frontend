02bba657db34cdba4c91bd5a7ae107780e8ee5fb
Aqib Owais <owaisharoon60@gmail.com>
2025-08-24 20:56:19 +0500

    deployement configs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  preview: {
-    allowedHosts: ['mingleup-frontend.onrender.com'],
-  },
+
 })
